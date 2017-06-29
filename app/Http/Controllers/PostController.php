421bf46e51d4da08732c68f84ae8b86b8a05d919
YiFan.Wu <jameswu1212@gmail.com>
2017-06-29 22:37:02 +0800

    New features: time of posting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Auth;
+use stdClass;
 
 
 class PostController extends Controller
@@ -17,7 +18,21 @@ class PostController extends Controller
     {
         // check user
         if (Auth::user()->username == $username) {
-            return view('/newpost');
+            $post = new stdClass();
+            $post->id = "";
+            $post->title = "";
+            $post->content = "";
+            $post->createDate = date("Y-m-d");
+            $post->updateDate = date("Y-m-d");
+
+            // two types of route in newpost.blade.php
+            $route = route('send_post', ['username' => $username]);
+            $routeDelete = "";
+
+            return view('/newpost')->with([
+                                        'post' => $post,
+                                        'route' => $route,
+                                        'routeDelete' => $routeDelete ]);
         }
     }
 
@@ -31,12 +46,11 @@ class PostController extends Controller
 
             // get current time
             $now = date('Y-m-d H:i:s');
-            $nowDate = date('Y-m-d');
 
             // get inputs from the form
-            $input = $request->only('post_title', 'post_content');
+            $input = $request->only('post_title', 'post_content', 'post_date', 'update_date');
 
-            $url = date('Ymd') * 7 . date('His') * 12;
+            $url = date('Ymd') * rand() - date('Hs'). date('His') * 12;
 
             DB::table('posts')->insert([
                 'u_id' => $userId,
@@ -44,8 +58,8 @@ class PostController extends Controller
                 'content' => $input['post_content'],
                 'created_at' => $now,
                 'updated_at' => $now,
-                'create_time' => $nowDate,
-                'update_time' => $nowDate,
+                'create_time' => $input['post_date'],
+                'update_time' => $input['update_date'],
                 'url' => $url
             ]);
 
@@ -121,7 +135,27 @@ class PostController extends Controller
                         ['u_id', '=', $input['u_id']]])
                     ->get();
 
-        return view('editpost')->with(['post' => $post[0]]);
+        // check wrong
+        $this->checkWrong(Auth::user()->id, $username);
+
+        // two types of route in editpost.blade.php
+        $route = route('update_post', [
+                            'username' => $username,
+                            'post_id' => $post[0]->id, 
+                            'u_id' => Auth::user()->id ]);
+        $routeDelete = route('delete_post', [
+                            'username' => $username,
+                            'post_id' => $post[0]->id, 
+                            'u_id' => Auth::user()->id ]);
+
+        // assign create and update values
+        $post[0]->createDate = $post[0]->create_time;
+        $post[0]->updateDate = $post[0]->update_time; 
+
+        return view('editpost')->with([
+                                    'post' => $post[0],
+                                    'route' => $route,
+                                    'routeDelete' => $routeDelete ]);
     }
 
 
@@ -129,10 +163,14 @@ class PostController extends Controller
     public function updatePost(Request $request, $username)
     {
         // get inputs from form
-        $input = $request->only('post_id', 'u_id', 'post_title', 'post_content');
+        $input = $request->only('post_id', 'u_id', 'post_title', 'post_content', 
+            'post_date', 'update_date');
 
         // get user id
         $userId = Auth::user()->id;
+
+        // get current time
+        $now = date("Y-m-d H:i:s");
         
         // updating
         DB::table('posts')
@@ -141,7 +179,10 @@ class PostController extends Controller
                 ['u_id', '=', $userId] ])
             ->update([
                 'title' => $input['post_title'],
-                'content' => $input['post_content']
+                'content' => $input['post_content'],
+                'create_time' => $input['post_date'],
+                'update_time' => $input['update_date'],
+                'updated_at' => $now
                 ]);
 
         // get url
--- resources/views/newpost.blade.php
+++ resources/views/newpost.blade.php
@@ -1,44 +1,33 @@
 @extends('main')
 
 @section('css-field')
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
+
     <style type="text/css">
         .area-edit {
             margin-bottom: 50px;
         }
     </style>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 @stop
 
 @section('container')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <h2 class="text-center">
-                @if (!isset($post))
+                @section('page-title')
                     發表新文章
-                    <? 
-                        $route = route('send_post', ['username' => Auth::user()->username]);
-                        $post = new stdClass();
-                        $post->title = "";
-                        $post->content = "";
-                    ?>
-                @else
-                    編輯文章
-                    <?  
-                        $route = route('update_post', [
-                                            'username' => Auth::user()->username,
-                                            'post_id' => $post->id, 
-                                            'u_id' => Auth::user()->id ]);
-                        $routeD = route('delete_post', [
-                                            'username' => Auth::user()->username,
-                                            'post_id' => $post->id, 
-                                            'u_id' => Auth::user()->id ]);  
-                    ?>
-                @endif      
+                @show     
             </h2>
             <div class="area-edit">
                 <form role="form" method="POST" action="{{ $route }}">
                     {{ csrf_field() }}
                     <div class="form-group">
-                        <label for="posttitle"><h4>文章標題</h4></label>
+                        <label for="post_title"><h4>文章標題</h4></label>
                         <input type="text" id="post_title" name="post_title" class="form-control" value="{{ $post->title }}"></input>
                     </div>
                     <div class="form-group">
@@ -46,14 +35,44 @@
                         <textarea id="post_content" name="post_content" class="form-control" rows="20">{{ $post->content }}</textarea>
                     </div>
                     <div class="form-group">
+                        <label for="post_date"><h4>發布日期</h4></label>
+                        <input type="text" id="post_date" name="post_date" class="form-control" style="width:auto" value="{{ $post->createDate }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="update_date"><h4>更新日期</h4></label>
+                        <input type="text" id="update_date" name="update_date" class="form-control" style="width:auto" value="{{ $post->updateDate }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="author"><h4>作者</h4></label>
+                        <input type="text" id="author" name="author" class="form-control" style="width:auto" value="{{ Auth::user()->username }}" disabled>
+                    </div>
+                    <div class="form-group">
                         <button type="submit" class="btn btn-primary">送出</button>
                     </div>
                 </form>
-                <form role="form" method="POST" action="{{ $routeD }}">
+                <form role="form" method="POST" action="{{ $routeDelete }}">
                     {{ csrf_field() }}
                     @yield('delete-button')
                 </form>
             </div>
         </div>
     </div>
+@stop
+
+@section('js-field')
+    <script type="text/javascript">
+        $(function() {
+            var postDate = new Date("{{ $post->createDate }}");
+            $('#post_date').datepicker({ 
+                dateFormat: 'yy-mm-dd',
+                defaultDate: postDate
+            });
+
+            var updateDate = new Date("{{ $post->updateDate }}");
+            $('#update_date').datepicker({ 
+                dateFormat: 'yy-mm-dd',
+                defaultDate: updateDate
+            });
+        });
+    </script>
 @stop
